58550851ea6d39891518cbd8aad5e44d9bc4a9c3
Jose Garcia Castillo <garcia.pepe10@gmail.com>
2023-02-23 19:12:11 +0100

    First commit

---
+++ src/main/java/guru/spring6framework/didemo/DidemoApplication.java
@@ -0,0 +1,13 @@
+package guru.spring6framework.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/spring6framework/didemo/DidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.spring6framework.didemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
